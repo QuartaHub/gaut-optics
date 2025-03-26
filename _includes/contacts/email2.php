789266f3fb22ee0ce29df8afa8abda16f06f5092
Vadim Sedelkin <v.sedelkin@quarta-hunt.ru>
2025-03-26 14:44:10 +0300

    fix: изменены адреса электронной почты и исправлены реквизиты юридического лица

--- _includes/contacts/email2.php
+++ _includes/contacts/email2.php
@@ -1,1 +1,1 @@
-<a class="contacts-info__text email_ico" href="mailto:shop@quarta-hunt.ru">quarta-hunt.ru</a>
+<a class="contacts-info__text email_ico" href="mailto:shop@gaut-optics.com">gaut-optics.com</a>
