2c2454d3826058a00100c0e1c9e2e4a4aa0ee647
Evgeniy Rusnak <rusnak@addamant.ru>
2025-08-06 11:57:49 +0600

    128610 | Вставить ссылки на социальные сети (gaut-optics.com)

--- _includes/contacts/social.php
+++ _includes/contacts/social.php
@@ -1,4 +1,4 @@
-<a class="ico vk" title="Вконтакте" target="_blank" href="https://vk.com/#">
+<a class="ico vk" title="Вконтакте" target="_blank" href="https://vk.com/quarta_hunt_official/">
 </a>
-<a class="ico tg" title="Telegram" target="_blank" href="https://t.me/#">
+<a class="ico tg" title="Telegram" target="_blank" href="https://t.me/quarta_hunt_official/">
 </a>
