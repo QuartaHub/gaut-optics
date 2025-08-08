05a231ad07c5566c44d93ddca906b625d032a892
Vadim Sedelkin <v.sedelkin@quarta-hunt.ru>
2025-08-08 10:17:56 +0300

    51619 | заменил номер телефона по вопросам гарантии

--- about/guaranty/index.php
+++ about/guaranty/index.php
@@ -1,28 +1,28 @@
 <?
-require($_SERVER["DOCUMENT_ROOT"]."/bitrix/header.php");
+require($_SERVER["DOCUMENT_ROOT"] . "/bitrix/header.php");
 $APPLICATION->SetTitle("Гарантия");
 ?>
 <div class="customers container with_bread">
-<div class="customers__content">
-<div class="text">
+	<div class="customers__content">
+		<div class="text">
 
-<h1>Гарантия и возврат</h1>
+			<h1>Гарантия и возврат</h1>
 
-<h2>Как получить гарантию на наш товар</h2>
+			<h2>Как получить гарантию на наш товар</h2>
 
-<p class="text">
-	 Отправьте товар и документы почтой или транспортной компанией СДЭК.
-</p>
-<p class="text">
-	 Что нужно делать:
-</p>
-<ul class="customers__list section-list">
-	<li class="section-list__item">Написать письмо с описанием проблемы на почтовый ящик <a href="mailto:service@gaut-optics.com">service@gaut-optics.com</a>. Добавить Ваш контактный телефон. Дождаться звонка менеджера. Он пришлет форму заявления и сориентирует по дальнейшим действиям</li>
-	<li class="section-list__item">Позвонить на федеральный номер <a href="tel:+7 (800) 775-28-68">+7 (800) 775-28-68</a> бесплатно и рассказать менеджеру о проблеме</li>
-</ul>
+			<p class="text">
+				Отправьте товар и документы почтой или транспортной компанией СДЭК.
+			</p>
+			<p class="text">
+				Что нужно делать:
+			</p>
+			<ul class="customers__list section-list">
+				<li class="section-list__item">Написать письмо с описанием проблемы на почтовый ящик <a href="mailto:service@gaut-optics.com">service@gaut-optics.com</a>. Добавить Ваш контактный телефон. Дождаться звонка менеджера. Он пришлет форму заявления и сориентирует по дальнейшим действиям</li>
+				<li class="section-list__item">Позвонить на федеральный номер <a href="tel:+78005550870">+7 (800) 555-08-70</a> бесплатно и рассказать менеджеру о проблеме</li>
+			</ul>
 
- 
- </div>
- </div>
+
+		</div>
+	</div>
 </div>
- <?require($_SERVER["DOCUMENT_ROOT"]."/bitrix/footer.php");?>
+<? require($_SERVER["DOCUMENT_ROOT"] . "/bitrix/footer.php"); ?>
